95f7883bf488de01a372219536f55cd69dcb779e
Enzo Soliz <enzosoliz95@gmail.com>
2023-02-03 18:26:31 -0300

    Configuracion de plugin y loaders para react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 
-const App = () => <h1>Heloo React!</h1>
+const App = () => <h1>Helloo React!!!!!</h1>
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filname: 'bundle.js'
+        filename: 'bundle.js'
     },
     resolve: {
         extensions: ['.js','.jsx']
@@ -17,12 +19,26 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {loader: 'html-loader'}
+                ]
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, "dist/"),
+        },
         compress: true,
-        port: 3006
-    }
+        port: 3006,
+    },
 }
